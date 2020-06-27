20bbdf5f8dd89d5e0fffffc2980496a2daeecf5e
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-06-27 11:14:32 -0700

    extracted mysql connection into separate module

--- sql_connect.js
+++ sql_connect.js
@@ -1,25 +1,9 @@
-const mysql = require("mysql");
+const connection = require("./assets/js/connection.js")
 const inquirer = require("inquirer");
 const consoleTable = require('console.table');
 const { query } = require("express");
 
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "employee_db"
-});
-
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    mainMenu();
-    // connection.end();
-});
-
-
+mainMenu();
 
 function mainMenu() {
     inquirer.prompt({
